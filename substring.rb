da00071fff34fb82284d77626d5dec6f367a30d4
forsmsmail2019 <113799599+forsmsmail2019@users.noreply.github.com>
2024-01-22 13:58:32 -0500

    lowercase all words

---
+++ substring.rb
@@ -0,0 +1,14 @@
+def checksubstring(word,dictionary)
+  #make dictionry and the word in lowercase
+  word.downcase!
+  dictionary.each do |word|
+    word.downcase!
+  end
+
+  puts word
+  puts dictionary
+  word.each do |c|
+    
+
+  end
+end
