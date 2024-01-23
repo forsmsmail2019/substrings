63fa5fed2b60667b1d23fcb39e6bfa92c115049b
forsmsmail2019 <113799599+forsmsmail2019@users.noreply.github.com>
2024-01-23 11:20:59 -0500

    try extract words for the word letters

--- substring.rb
+++ substring.rb
@@ -1,21 +1,42 @@
 def checksubstring(word,dictionary)
   #make dictionry and the word in lowercase
   word.downcase!
+  puts word
   dictionary.each do |word|
     word.downcase!
   end
+  #check words
+  #try to extract words from the word
+  tmpword=word[0]
+  j= 0
+  for i in 0..word.length
+    for j in 1..word.length-1
+      tmpword += word[j]
+      puts tmpword
+      if i == word.length-1
+        i +=1
+        tmpword
+      end
+      #if dictionary.include?()
 
-  word.each do |c|
-
-
+      #end
+    end
   end
 
 
-  
+
+
+
+
+
+
+
+
 
 end
 
 
 dic = "amine,nassim,dahmoun".split(",")
-puts dic
+print dic
+
 checksubstring("amiNe",dic)
