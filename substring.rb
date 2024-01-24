5a209b07ce16cc31c1e8d25afdd6e125d36cf976
forsmsmail2019 <113799599+forsmsmail2019@users.noreply.github.com>
2024-01-24 12:10:54 -0500

    fix no

--- substring.rb
+++ substring.rb
@@ -1,63 +1,50 @@
-$substrings={}
 def substring(word,dictionary)
   #make dictionry and the word in lowercase
-  word.downcase!
+  #convert the given word or text to array
+  word = word.split(" ")
+  word.each { |value| value.downcase! }
   puts word
-  dictionary.each do |word|
-    word.downcase!
-  end
-  #check words
-  #try to extract words from the word
+  # dictionary.each do |word|
+  #   word.downcase!
+  # end
 
+
+  substrings = Hash.new(0)
   i = 0
-  while i < word.length do
+  while i < dictionary.length do
+    word.each do |value|
+      value.include?(dictionary[i])
+      puts "this: #{dictionary[i]} found"
+      substrings[dictionary[i]] += 1
+    end
+      # if word.include?(dictionary[i])
+      #   puts "this: #{dictionary[i]} found"
+      #   substrings[dictionary[i]] += 1
+
+      # end
+
+
 
       #tmpword = word[i]
       #puts tmpword
       #puts "i : #{i}"
-      #puts "j : #{j}"
       #check the generated word ifexiste in dicion
-      if dictionary.include?(word[i])
-        #puts "i : #{i}"
-        #puts "j : #{j}"
-        #puts "this :#{tmpword} found"
-        unless $substrings.include?(:word)
-          $substrings[word[i]]=1
-        else
-          $substrings[word[i]]+=1
-        end
-        #puts $substrings
-      end
+
       i+= 1
 
   end
+
+  puts substrings
 end
 
-#clear the entered text from special chars
-#convert the text to words in array
-def cleartxt(text)
-  text.delete!(",?':;")
-  #puts text
-  words = text.split(" ")
-  #puts words
-  return words
-end
 
-def check(text,dic)
-  words = cleartxt(text)
-  for word in words
-    substring(word,dic)
-  end
 
-  puts $substrings
-
-end
 
 
 
 
 #dictionnary of words
-dic = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dic = ["below","down","go","o","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 #print dic
 #call the method checksubsting
-check("Howdy partner, sit down! How's it going?",dic)
+substring("belOw",dic)
