b443108c211753b5050490a7b664b1fdc12dc95d
forsmsmail2019 <113799599+forsmsmail2019@users.noreply.github.com>
2024-01-24 12:23:27 -0500

    fix bug ,clean the code

--- substring.rb
+++ substring.rb
@@ -4,47 +4,30 @@ def substring(word,dictionary)
   word = word.split(" ")
   word.each { |value| value.downcase! }
   puts word
-  # dictionary.each do |word|
-  #   word.downcase!
-  # end
+  
+  dictionary.each do |word|
+    word.downcase!
+  end
 
 
   substrings = Hash.new(0)
-  i = 0
-  while i < dictionary.length do
-    word.each do |value|
-      value.include?(dictionary[i])
-      puts "this: #{dictionary[i]} found"
-      substrings[dictionary[i]] += 1
-    end
-      # if word.include?(dictionary[i])
-      #   puts "this: #{dictionary[i]} found"
-      #   substrings[dictionary[i]] += 1
 
-      # end
-
-
-
-      #tmpword = word[i]
-      #puts tmpword
-      #puts "i : #{i}"
-      #check the generated word ifexiste in dicion
-
+  word.each do |value|
+    i = 0
+    while i < dictionary.length do
+      if (value.include?(dictionary[i]))
+        puts "this: #{dictionary[i]} found"
+        substrings[dictionary[i]] += 1
+      end
       i+= 1
-
+    end
   end
 
   puts substrings
 end
 
-
-
-
-
-
-
 #dictionnary of words
 dic = ["below","down","go","o","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 #print dic
 #call the method checksubsting
-substring("belOw",dic)
+substring("Howdy partner, sit down! How's it going?",dic)
